b38ac130092d6b4cddf8e7c05d9dd5e9671b1cac
Cyprien <cyprien@intredys.fr>
2024-05-06 15:53:28 +0200

    Update ubooquity.sh

--- ubooquity.sh
+++ ubooquity.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# Compatible Debian 9 Stretch
+# Compatible Debian 9 Stretch // Ubuntu 16.04
 # GPL
-#
-# Syntaxe: # su - -c "./debian-postinstall.sh"
-# Syntaxe: or # sudo ./debian-postinstall.sh
+
 VERSION="0.1"
 
 # Test que le script est lance en root
@@ -54,21 +52,16 @@ if [ ! -d "/home/tv" ];then
 		cd ubooquity
 fi	
 
-# Download Ubooquity	
+# Download Ubooquity
+#-----------------------------------
 wget -O ubooquity.zip http://vaemendis.net/ubooquity/service/download.php
 unzip -o ubooquity.zip && rm ubooquity.zip
 
 # Starting Ubooquity
+#-----------------------------------
 iptables -A INPUT -p tcp -m tcp --dport 4048 -j ACCEPT
 screen java -jar -Djava.awt.headless=true /home/tv/ubooquity/Ubooquity.jar -webadmin -headless -port 4048
 
 echo 'Enter one line at a time'
 echo 'Press CTRL A+D to finish'
 cat >test.out
-
-# Themes
-cd /tmp
-
-wget -O  "Dark Theme - Ubooquity.zip" "http://bit.ly/1FvhCZZ" && unzip Dark*.zip -d /home/tv/ubooquity/themes/DarkThemes 
-wget -O http://vaemendis.net/ubooquity/downloads/themes/modern-1.7.0.zip && unzip modern-1.7.0.zip -d /home/tv/ubooquity/themes/
-wget -O TheGray.zip "https://docs.google.com/uc?id=0B2UyNSVyI_nRbzNZak9pUFZQQ2M&export=download" && unzip TheGray.zip -d /home/tv/ubooquity/themes/
